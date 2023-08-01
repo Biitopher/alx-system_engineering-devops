ffb01542e107e4e910c53b5fa01aaa8708f7b4e4
Biitopher <biitopher@gmail.com>
2023-08-01 08:35:52 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #Ruby script that output: [SENDER],[RECEIVER],[FLAGS]
 
-puts ARGV[0].scan(/[SENDER],[RECEIVER],[FLAGS]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')

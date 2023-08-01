7215868073f3da355a120466a4eafed4420cedca
Biitopher <biitopher@gmail.com>
2023-08-01 05:50:01 -0700

    Beginning and end

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #Ruby script accepting one argument and pass to regular expression starts with h ends with n and single character in between
 
-puts ARGV[0].scan(/\b(^h.n$)\b/).join
+puts ARGV[0].scan(/\b(^h.n$)\b/)).join

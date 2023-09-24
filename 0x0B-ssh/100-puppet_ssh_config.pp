656989bd6deb95af2baf80c1fc6475c5fbd09999
Biitopher <biitopher@gmail.com>
2023-09-24 14:37:50 -0700

    Task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#Changes to our configuration file using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
+file_line { 'Turn off password auth':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'PasswordAuthentication no',
+  match =>  '^#PasswordAuthentication',
+}
+
+file_line { 'Delete identify file':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'IdentifyFile ~/.ssh/school',
+  match =>  '^#IdentifyFile',
+}

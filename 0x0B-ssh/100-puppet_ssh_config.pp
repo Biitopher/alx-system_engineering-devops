6176eb03c61c1cd3b54cd10cf102d0183198ac9e
Biitopher <biitopher@gmail.com>
2023-09-24 15:55:36 -0700

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,17 +2,16 @@
 #Changes to our configuration file using Puppet
 
 file { '/etc/ssh/ssh_config':
-  ensure => present,
+  ensure => present
 }
 
 file_line { 'Turn off password auth':
-  path  =>  '/etc/ssh/ssh_config',
-  line  =>  'PasswordAuthentication no',
-  match =>  '^#PasswordAuthentication',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
-
-file_line { 'Declare identify file':
-  path  =>  '/etc/ssh/ssh_config',
-  line  =>  'IdentifyFile ~/.ssh/school',
-  match =>  '^#IdentifyFile',
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }

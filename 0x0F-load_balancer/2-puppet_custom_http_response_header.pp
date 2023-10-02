3ac2ac7f9b4f1c99c8899f6f1e5913a3bfe8a564
biitopher <biitopher@gmail.com>
2023-10-02 15:50:38 -0700

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,17 @@
 #!/usr/bin/env bash
 #custom HTTP header with Puppet
 
-class custom_http_response_header {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/custom-header':
-    ensure  => present,
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    add_header X-Served-By $::hostname;
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-",
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-enabled/custom-header':
-    ensure  => 'link',
-    target  => '/etc/nginx/sites-available/custom-header',
-    require => File['/etc/nginx/sites-available/custom-header'],
-  }
-
-  exec { 'reload_nginx':
-    command     => '/usr/sbin/service nginx reload',
-    refreshonly => true,
-    subscribe   => [File['/etc/nginx/sites-available/custom-header'], File['/etc/nginx/sites-enabled/custom-header']],
-  }
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
 }

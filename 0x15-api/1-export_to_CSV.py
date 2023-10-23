69a4346ee8bc570dda306f258037f927ab4f455f
biitopher <biitopher@github.com>
2023-10-23 10:49:26 -0700

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
 """Python script using REST API and exporting data to CSV"""
 
-import requests
-import sys
-import csv
+if __name__ == "__main__":
 
-if __name__ == "__main":
-
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    import csv
+    import requests
+    import sys
 
     userId = sys.argv[1]
-
-    user_response = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
-
-    if user_response.status_code != 200:
-        print(f"Error: User with ID {userId} not found.")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    user_name = user_data.get('name')
-
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    user_name = user.json().get('username')
     todos_response = requests.get(
-            "https://jsonplaceholder.typicode.com/todos")
-    todo_data = todos_response.json()
+            'https://jsonplaceholder.typicode.com/todos')
 
-    filename = userId + ".csv"
-    with open(filename, mode='w', newline='') as csv_file:
+    filename = userId + '.csv'
+    with open(filename, mode='w') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
                                 quoting=csv.QUOTE_ALL, lineterminator='\n')
-
-        for task in todos_data:
+        for task in todos_response.json():
             if task.get('userId') == int(userId):
-                writer.writerow([userId, user_name, str(task.get(
+                csv_writer.writerow([userId, user_name, str(task.get(
                     'completed')), task.get('title')])

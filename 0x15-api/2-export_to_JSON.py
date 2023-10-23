0ceda7dfcfdb5475f531c165263b3bc64b48eb5a
biitopher <biitopher@github.com>
2023-10-23 04:06:10 -0700

    Task 1

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,32 +12,26 @@ if __name__ == "__main":
 
     userId = sys.argv[1]
 
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+    user_response = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{userId}")
 
     if user_response.status_code != 200:
         print(f"Error: User with ID {userId} not found.")
         sys.exit(1)
 
-    user_data = user_response.json()
-    user_name = user_data.get('name')
+    user_name = user_response.json().get('name')
 
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={userId}")
-    todo_data = todos_response.json()
-
-    totalTasks = len(todo_data)
-    completed = sum(1 for task in todo_data if task.get('completed'))
-
-    print('Employee {} is done with tasks({}/{}):'.format(user_name, completed, totalTasks))
-
-    for task in todo_data:
-        if task.get('completed'):
-            print('\t' + task.get('title'))
+    todos_response = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={userId}")
 
     filename = f"{userId}.csv"
     with open(filename, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todo_data:
-            csv_writer.writerow([userId, user_name, task.get('completed'), task.get('title')])
+        csv_writer.writerow(["USER_ID", "USERNAME",
+                            "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todos_response.json():
+            csv_writer.writerow(
+                    [userId, user_name,
+                     task.get('completed'), task.get('title')])
 
     print(f"Data has been exported to {filename} in CSV format.")

01d6fe6ec942700906209d4124a679374b1c988f
Biitopher <biitopher@gmail.com>
2023-11-07 14:52:36 +0300

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Queries Reddit and prints titles"""
+import requests
+import sys
+import json
+
+
+def top_ten(subreddit):
+    """Define the Reddit API"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
+    headers = {
+        'User-Agent': 'MyRedditBot/1.0'
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if 'data' in data and 'children' in data['data']:
+                posts = data['data']['children']
+                for i, post in enumerate(posts, 1):
+                    print(f"{i}. {post['data']['title']}")
+            else:
+                print("No posts found in this subreddit.")
+        elif response.status_code == 302:
+            print("Subreddit is not valid.")
+        else:
+            return (None)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")

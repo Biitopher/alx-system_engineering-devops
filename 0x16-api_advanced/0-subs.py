604db1f55d11d9884bb4d1bb2fe68b2111a866ef
biitopher <biitopher@github.com>
2023-11-07 03:24:26 -0800

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {
+        'User-Agent': 'MyRedditBot/1.0'
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            subscribers = data['data']['subscribers']
+            return subscribers
+        elif response.status_code == 302:
+            return 0
+        else:        
+            return 0
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return 0

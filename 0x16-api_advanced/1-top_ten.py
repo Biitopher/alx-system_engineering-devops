72c7b0e296e7791b1e77cd68c1e03644dd474d78
Biitopher <biitopher@gmail.com>
2023-11-07 17:12:02 +0300

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Queries Reddit and prints titles"""
 import requests
-import sys
-import json
 
 
 def top_ten(subreddit):
@@ -12,22 +10,14 @@ def top_ten(subreddit):
     headers = {
         'User-Agent': 'MyRedditBot/1.0'}
 
-    try:
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
             data = response.json()
-            if 'data' in data and 'children' in data['data']:
-                posts = data['data']['children']
-                for i, post in enumerate(posts, 1):
-                    print(f"{i}. {post['data']['title']}")
-            else:
-                print("No posts found in this subreddit.")
-        elif response.status_code == 302:
-            print("Subreddit is not valid.")
-            return None
-
+            for get_data in data.get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+            
         else:
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+                print(None)

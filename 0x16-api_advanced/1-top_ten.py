33187842790553c8b2c7e40abd9a673a384ec819
Biitopher <biitopher@gmail.com>
2023-11-07 17:23:25 +0300

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,19 +5,16 @@ import requests
 
 def top_ten(subreddit):
     """Define the Reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        parameter={"limit": 10},
+    )
 
-    headers = {
-        'User-Agent': 'MyRedditBot/1.0'}
-
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            data = response.json()
-            for get_data in data.get("data").get("children"):
-            dat = get_data.get("data")
+    if response.status_code == 200:
+        for req_data in response.json().get("data").get("children"):
+            dat = req_data.get("data")
             title = dat.get("title")
             print(title)
-            
-        else:
-                print(None)
+    else:
+        print(None)

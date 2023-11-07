715b0accbfdb1ebb006cfc769c50e7e711537b55
Biitopher <biitopher@gmail.com>
2023-11-07 22:02:34 +0300

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,42 +3,32 @@
 import requests
 
 
-def words_count(subreddit, word_list, after=None, counts=None):
-    """Define the Reddit API word count"""
-    response = requests.get(
-        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False)
-
-    if response.status_code != 200:
-        return('None')
-
-    data = response.json()
-
-    posts = [child.get("data").get("title")
-             for child in data
-             .get("data")
-             .get("children")]
-    if not posts:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if count_word == {}:
+def words_count(subreddit, word_list, after=None, count_word=None):
+    if count_word is None:
         count_word = {word: 0 for word in word_list}
 
-    for title in posts:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    count_word[word] += 1
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10&after={after}",
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
 
-    if not data.get("data").get("after"):
-        counts_sorted = sorted(count_word.items(), key=lambda kv: kv[0])
-        counts_sorted = sorted(count_word.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in counts_sorted if v != 0]
-    else:
-        return words_count(subreddit, word_list, count_word,
-                           data.get("data").get("after"))
+    if response.status_code != 200:
+        return 'None'
+
+    data = response.json()
+    posts = [child.get("data").get("title") for child in data.get("data").get("children")]
+
+    for title in posts:
+        split_words = title.lower().split()  # Convert to lowercase for case-insensitive matching
+        for word in word_list:
+            if word.lower() in split_words:
+                count_word[word] += split_words.count(word.lower())
+
+    if data.get("data").get("after"):
+        return words_count(subreddit, word_list, data.get("data").get("after"), count_word)
+    
+    counts_sorted = sorted(count_word.items(), key=lambda kv: (-kv[1], kv[0]))
+    for word, count in counts_sorted:
+        if count > 0:
+            print(f"{word}: {count}")

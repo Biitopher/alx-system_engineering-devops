e35c4f42d8d147930f42a648e1301c914c1f3b86
Biitopher <biitopher@gmail.com>
2023-11-08 00:12:08 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,30 +2,15 @@
 """Exports cvs files"""
 
 import requests
-import sys
-import json
 
 
 def number_of_subscribers(subreddit):
     """defines reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
 
-    headers = {
-        'User-Agent': '0-subs'
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            data = response.json()
-
-            subscribers = data['data']['subscribers']
-            return subscribers
-        elif response.status_code == 302:
-            return (0)
-        else:
-            return (0)
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return (0)
+    return response.json().get("data").get("subscribers")

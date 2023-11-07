74cd25543559a03642ecca1240ed695c126cc387
Biitopher <biitopher@gmail.com>
2023-11-07 22:55:18 +0300

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,33 +3,39 @@
 import requests
 
 
-def words_count(subreddit, word_list, after=None, count_word=None):
+def words_count(subreddit, word_list, after=None, word_count={}):
     """Defines words count"""
-    if count_word is None:
-        count_word = {word: 0 for word in word_list}
-
     response = requests.get(
         f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10&after={after}",
         headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False
-    )
+        allow_redirects=False)
 
     if response.status_code != 200:
         return 'None'
 
     data = response.json()
-    posts = [child.get("data").get("title") for child in data.get("data").get("children")]
+    posts = [child.get("data").get("title")
+     for child in data.get("data").get("children")]
+    if not posts:
+        return None
+
+    word_list = list(dict.fromkeys(word_list))
+
+    if word_count == {}:
+        word_count = {word: 0 for word in word_list}
 
     for title in posts:
-        split_words = title.lower().split()  # Convert to lowercase for case-insensitive matching
+        split_words = title.split(' ')
         for word in word_list:
-            if word.lower() in split_words:
-                count_word[word] += split_words.count(word.lower())
+            for s_word in split_words:
+                if s_word.lower() == word.lower():
+                    word_count[word] += 1
 
-    if data.get("data").get("after"):
-        return words_count(subreddit, word_list, data.get("data").get("after"), count_word)
-    
-    counts_sorted = sorted(count_word.items(), key=lambda kv: (-kv[1], kv[0]))
-    for word, count in counts_sorted:
-        if count > 0:
-            print(f"{word}: {count}")
+    if not data.get("data").get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
+        sorted_counts = sorted(word_count.items(),
+                               key=lambda kv: kv[1], reverse=True)
+        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+    else:
+        return count_words(subreddit, word_list, word_count,
+                           data.get("data").get("after"))

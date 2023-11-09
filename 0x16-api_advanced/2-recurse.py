df20b0abee78bd141fd74db9dab7864ad4677f2b
Biitopher <biitopher@gmail.com>
2023-11-10 01:07:55 +0300

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,20 +9,24 @@ def recurse(subreddit, hot_list=[], after=""):
    if hot_list is None:
         hot_list = []
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': '2-recurse'}
+    headers = {
+        'User-Agent': '2-recurse'
+    }
     params = {'after': after} if after else {}
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
         try:
-            data = response.json()['data']['children'][:10]
-            for post in data:
+            post_data = response.json()['data']['children'][:10]
+            for post in post_data:
                 title = post['data']['title']
                 hot_list.append(title)
             after = response.json()['data']['after']
             if after is not None:
+                # Recursively call the function for the next page of results
                 return recurse(subreddit, hot_list, after)
             else:
+                # No more pages, return the hot_list
                 return hot_list
         except (KeyError, ValueError):
             return None

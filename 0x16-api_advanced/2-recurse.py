74e1ed501c18690cfd61a3db2c1169817d4c8216
Biitopher <biitopher@gmail.com>
2023-11-10 01:04:37 +0300

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
 """Recursive function that queries the Reddit API"""
+import sys
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
     """defines Reddit API"""
-    response = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"after": after},
-    )
+   if hot_list is None:
+        hot_list = []
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': '2-recurse'}
+    params = {'after': after} if after else {}
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
-        for get_data in response.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            hot_list.append(title)
-        after = response.json().get("data").get("after")
-
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
+        try:
+            data = response.json()['data']['children'][:10]
+            for post in data:
+                title = post['data']['title']
+                hot_list.append(title)
+            after = response.json()['data']['after']
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        except (KeyError, ValueError):
+            return None
+    elif response.status_code == 302:
+        return None
     else:
         return None
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        subreddit = sys.argv[2]
+        recurse(subreddit)

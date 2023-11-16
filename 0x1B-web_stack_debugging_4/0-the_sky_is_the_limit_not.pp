f1675423cf3fe6310fc9fab995b41616baa49907
biitopher <biitopher@gmail.com>
2023-11-16 15:48:13 +0000

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Puppet script
-exec { 'set limit':
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+exec { 'set_limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
 # Nginx restart
--> exec { 'reboot nginx':
+-> exec { 'reboot_nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
